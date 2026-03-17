20b4edf4af2d3899640dc1eb9fc25cce93407511
Shivasai4 <ganjishivasai4@gmail.com>
2026-03-17 23:41:55 +0530

    update project

--- app/book-test/page.tsx
+++ app/book-test/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react"
 import BookingRequestForm from "@/components/booking/BookingRequestForm"
 
 export default function BookTestPage() {
-  return <BookingRequestForm />
+  return (
+    <Suspense fallback={<section className="bg-background py-16 lg:py-20" />}>
+      <BookingRequestForm />
+    </Suspense>
+  )
 }
